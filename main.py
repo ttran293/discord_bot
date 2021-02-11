a001698214a2f87b99e7ee0e40440781a15913fb
Thanh Nam Tran <ttran19@umbc.edu>
2021-02-11 12:04:30 -0500

    Fix en make hello function

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
+
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
+
 @client.event
 async def on_ready():
-    print('We have loggin as {0.user'.format(client))
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
-
-#Test modified
+client.run(TOKEN)
